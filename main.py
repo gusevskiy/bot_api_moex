e314ef7f88657b4b5439cb1b7ddf90724d361280
gusevskiy <vlgu@mail.ru>
2023-01-29 01:02:08 +0300

    Первый черновой рабочий вариант. Поиск по части названия. Вывод нескольких эмитентов если совпадение не одно.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+# coding: utf-8
 import requests
 import os
 
 from dotenv import load_dotenv
 import telegram
-from telegram import Bot
+from telegram.ext import Updater, MessageHandler, Filters
 
 
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+updater = Updater(token=TELEGRAM_TOKEN)
 
 
 def check_tokens() -> bool:
@@ -18,24 +21,42 @@ def check_tokens() -> bool:
     return all([TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
-def search_ticker_name_close_prise(name: str) -> tuple:
+def search_ticker_name_close_prise(name: str) -> list:
     """
     This function search ticker, name, price
     name: here in Cyrillic
     """
+    data = []
     url = "https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR" \
           "/securities.json?iss.meta=off&iss.only=securities&securities" \
           ".columns=SECID,PREVADMITTEDQUOTE,SECNAME"
     all_tickers = requests.get(url).json().get('securities')['data']
     for ticker in all_tickers:
-        if name in ticker[2]:
-            return ticker
+        if name.lower() in ticker[2].lower():
+            data.append(ticker)
+    return data
 
 
-# print("search_ticker_name_close_prise", search_ticker_name_close_prise("Сбер"))
+# def send_message(bot: telegram.bot.Bot, message: str) -> None:
+#     """Send message in telegram."""
+#     bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
-def send_message(bot: telegram.Bot, ticker: str) -> None:
-    """Send message in telegram."""
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=ticker)
+def main(update, context):
+    chat = update.effective_chat
+    text_ticker = update.message.text
+    data = list(search_ticker_name_close_prise(text_ticker))
+    for message in data:
+        context.bot.send_message(
+            chat_id=chat.id,
+            text=f'{message[2]}, Цена 1й акции = {message[1]} руб.')
 
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, main))
+
+updater.start_polling()
+updater.idle()
+
+# if __name__ == '__main__':
+#     main('Сбер')
+    # print(search_ticker_name_close_prise('Сбер')[0][2])
--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-import requests
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
-url = "https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR" \
-      "/securities.json?iss.meta=off&iss.only=securities&securities.columns" \
-      "=SECID,PREVADMITTEDQUOTE,SECNAME"
-all_tickers = requests.get(url)#.json().get('securities')['data']
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+YOUR_TOKEN_HERE = '5891863496:AAF2MemwhNDKNBX3Ok7BcBwmC48SXHj4C6s'
 
-print(all_tickers.json())
+app = ApplicationBuilder().token(YOUR_TOKEN_HERE).build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
