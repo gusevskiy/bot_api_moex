e28e980d6554357f51048ca6f93e71ba7a0149ca
gusevskiy <vlgu@mail.ru>
2023-01-29 23:58:43 +0300

    натворил с гитом какой то хрени. Это основная рабочая версия

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+# coding: utf-8
 import requests
 import os
 
 from dotenv import load_dotenv
-<<<<<<< HEAD
 import telegram 
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
-=======
-import telegram
-from telegram import Bot
->>>>>>> parent of e314ef7 (Первый черновой рабочий вариант.)
 
 
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+updater = Updater(token=TELEGRAM_TOKEN)
 
 
 def check_tokens() -> bool:
@@ -23,21 +21,22 @@ def check_tokens() -> bool:
     return all([TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
-def search_ticker_name_close_prise(name: str) -> tuple:
+def search_ticker_name_close_prise(name: str) -> list:
     """
     This function search ticker, name, price
     name: here in Cyrillic
     """
+    data = []
     url = "https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR" \
           "/securities.json?iss.meta=off&iss.only=securities&securities" \
           ".columns=SECID,PREVADMITTEDQUOTE,SECNAME"
     all_tickers = requests.get(url).json().get('securities')['data']
     for ticker in all_tickers:
-        if name in ticker[2]:
-            return ticker
+        if name.lower() in ticker[2].lower():
+            data.append(ticker)
+    return data
 
 
-<<<<<<< HEAD
 def main(update, context):
     chat = update.effective_chat
     text_ticker = update.message.text
@@ -55,11 +54,12 @@ def say_hi(update, context):
         chat_id=chat.id,
         text=(f'Привет {name}! \n' 
         f'бот ищет по названию все совпадения в названиях компаний которые торгуются на ММВБ в режиме TQBR (T+2) и предоставляет цены закрытия предыдущей торговой сессии, также доходность за текущий год и доходность от момента покупки год назад.\n Введи любое название которое знаешь, например: "Сбер".'))
-    # updater.bot.answer_callback_query(callback_query_id=chat, show_alert=True, text='привет', cache_time=100)
+
 
 # CommandHandler должен находится выше
 updater.dispatcher.add_handler(CommandHandler('help', say_hi))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, main))
+
 
 
 updater.start_polling()
@@ -68,12 +68,3 @@ updater.idle()
 # if __name__ == '__main__':
 #     main('Сбер')
     # print(search_ticker_name_close_prise('Сбер')[0][2])
-=======
-# print("search_ticker_name_close_prise", search_ticker_name_close_prise("Сбер"))
-
-
-def send_message(bot: telegram.Bot, ticker: str) -> None:
-    """Send message in telegram."""
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=ticker)
-
->>>>>>> parent of e314ef7 (Первый черновой рабочий вариант.)
